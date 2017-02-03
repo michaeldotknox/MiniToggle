85f98abceaa29057117cb0be4e0a969883eae63a
MichaelDotKnox <michael.knox@michaeldotknox.com>
2017-02-02 21:14:21 -0600

    Added the ability to cache toggles

---
+++ Source/MiniToggle.Core/CachedToggle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiniToggle.Core
+{
+    /// <summary>
+    /// Indicates that the toggle will be cached
+    /// </summary>
+    public class CachedToggle
+    {
+        internal Type Toggle { get; set; }
+    }
+}
